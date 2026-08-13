15e9ba09a5349651771a858d1df3e870f240c0d7
Lavit Saini <lavitsaini04@gmail.com>
2026-08-13 21:50:19 +0530

    add frontend domain in cors settings

--- app/main.py
+++ app/main.py
@@ -20,8 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://class-face.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
